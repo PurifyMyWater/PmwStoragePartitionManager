b2b3d136520dddcaf6f385aa5027085ef4e8c564
vacmg <70143287+vacmg@users.noreply.github.com>
2025-05-07 21:46:14 +0200

    Fix CI

--- test/test.cpp
+++ test/test.cpp
@@ -1,7 +1,7 @@
 #include <unity.h>
 #include <StoragePartitionManager.h>
 
-TEST_CASE("getConf", "[PMW-StoragePartitionManagerComponent]")
+TEST_CASE("getConf", "[PMW-StoragePartitionManager]")
 {
     StoragePartitionManager::getConf();
     TEST_ASSERT_TRUE(true);
